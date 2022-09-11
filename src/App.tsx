f74b5fe0834c258a28edadb7cd706d6928d3ff0c
Marius Andra <marius.andra@gmail.com>
2022-09-12 01:03:56 +0200

    initial import

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import reactLogo from "./assets/react.svg"
+import "./App.css"
+import { actions, kea, path, reducers, useActions, useValues } from "kea"
+
+import type { appLogicType } from "./AppType"
+
+export const appLogic = kea<appLogicType>([
+  path(["src", "App"]),
+  actions({ setCount: (value: number) => ({ value }) }),
+  reducers({ count: [0, { setCount: (_, { value }) => value }] }),
+])
+
+function App() {
+  const { count } = useValues(appLogic)
+  const { setCount } = useActions(appLogic)
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://keajs.org" target="_blank">
+          <img src="/kea.svg" className="logo" alt="Kea logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + Kea + React</h1>
+      <div className="card">
+        <button onClick={() => setCount(count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">Click on the Vite, Kea and React logos to learn more</p>
+    </div>
+  )
+}
+
+export default App

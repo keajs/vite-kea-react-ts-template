cace6b0f25c7b4606f748ab42900facb41abebb6
Marius Andra <marius.andra@gmail.com>
2022-09-18 21:17:23 +0200

    add kea-router to demo

--- src/appLogic.ts
+++ src/appLogic.ts
@@ -1,9 +1,30 @@
 import { actions, kea, path, reducers } from 'kea'
+import { actionToUrl, urlToAction } from 'kea-router'
 
 import type { appLogicType } from './appLogicType'
 
 export const appLogic = kea<appLogicType>([
   path(['src', 'App']),
-  actions({ setCount: (value: number) => ({ value }) }),
-  reducers({ count: [0, { setCount: (_, { value }) => value }] }),
+  actions({
+    setCount: (count: number) => ({ count }),
+  }),
+  reducers({
+    count: [
+      0,
+      {
+        setCount: (_, { count }) => count,
+      },
+    ],
+  }),
+  actionToUrl({
+    setCount: ({ count }) => count === 0 ? '/' : `/${count}`,
+  }),
+  urlToAction(({ actions, values }) => ({
+    '/(:count)': ({ count }) => {
+      const newCount = parseInt(count || '0')
+      if (values.count !== newCount) {
+        actions.setCount(newCount)
+      }
+    },
+  })),
 ])

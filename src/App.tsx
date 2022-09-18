704154c8639b29de75f3503378d8995bf69a4079
Marius Andra <marius.andra@gmail.com>
2022-09-18 20:39:57 +0200

    back to 2 spaces

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,30 @@ import { useActions, useValues } from 'kea'
 import { appLogic } from './appLogic'
 
 export function App() {
-    const { count } = useValues(appLogic)
-    const { setCount } = useActions(appLogic)
+  const { count } = useValues(appLogic)
+  const { setCount } = useActions(appLogic)
 
-    return (
-        <div className="App">
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://keajs.org" target="_blank">
-                    <img src="/kea.svg" className="logo" alt="Kea logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + Kea + React</h1>
-            <div className="card">
-                <button onClick={() => setCount(count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite, Kea and React logos to learn more</p>
-        </div>
-    )
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://keajs.org" target="_blank">
+          <img src="/kea.svg" className="logo" alt="Kea logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + Kea + React</h1>
+      <div className="card">
+        <button onClick={() => setCount(count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">Click on the Vite, Kea and React logos to learn more</p>
+    </div>
+  )
 }
--- src/appLogic.ts
+++ src/appLogic.ts
@@ -3,7 +3,7 @@ import { actions, kea, path, reducers } from 'kea'
 import type { appLogicType } from './appLogicType'
 
 export const appLogic = kea<appLogicType>([
-    path(['src', 'App']),
-    actions({ setCount: (value: number) => ({ value }) }),
-    reducers({ count: [0, { setCount: (_, { value }) => value }] }),
+  path(['src', 'App']),
+  actions({ setCount: (value: number) => ({ value }) }),
+  reducers({ count: [0, { setCount: (_, { value }) => value }] }),
 ])
